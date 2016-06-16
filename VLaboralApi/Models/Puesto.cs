337ab7047bfea6ef55bc260487dcfab4d294fe2b
iafar86 <iafar86@hotail.com>
2016-06-16 17:32:22 -0300

    Correcciones de tabulacion

--- VLaboralApi/Models/Puesto.cs
+++ VLaboralApi/Models/Puesto.cs
@@ -11,24 +11,24 @@ namespace VLaboralApi.Models
         {
             this.Subrubros = new HashSet<SubRubro>(); //fpaz: para la relacion M a M coin subrubros
         }
-        
+
         public int Id { get; set; }
         public string Nombre { get; set; }
         public string Descripcion { get; set; }
         public string Ubicacion { get; set; }
         public ICollection<string> Habilidades { get; set; }
         public string Remuneracion { get; set; }
-	public int Vacantes { get; set; } //fpaz: numero de vacantes para el puesto
-	
+        public int Vacantes { get; set; } //fpaz: numero de vacantes para el puesto
+
         //iafar: Relacion 1 a M con Oferta (1)
         public int OfertaId { get; set; }
         public Oferta Oferta { get; set; }
-        
+
         //fpaz: relacion M a M con Subrubros
         public virtual ICollection<SubRubro> Subrubros { get; set; }
 
         //fpaz: relacion 1 a M con Tipo Contrato (uno)
-        public int TipoContratoId { get; set; }        
+        public int TipoContratoId { get; set; }
         public virtual TipoContrato TipoContrato { get; set; }
 
         //fpaz: relacion 1 a M con Tipo Disponibilidad (uno)
