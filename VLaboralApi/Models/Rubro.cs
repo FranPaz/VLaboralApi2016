626550c6945444e9682a77e3495dc0efe8d5cec4
santiluna <santi_luna@hotmail.com>
2016-06-16 02:06:04 -0300

    Model Rubro Subrubros y relación con Puesto

--- VLaboralApi/Models/Rubro.cs
+++ VLaboralApi/Models/Rubro.cs
@@ -5,14 +5,10 @@ using System.Web;
 
 namespace VLaboralApi.Models
 {
-    public class Puesto
+    public class Rubro
     {
         public int Id { get; set; }
         public string Nombre { get; set; }
         public string Descripcion { get; set; }
-        public string Ubicacion { get; set; }
-        public ICollection<string> Habilidades { get; set; }
-        public string Remuneracion { get; set; }
-        public Subrubro Subrubro { get; set; }
     }
 }
---
+++ VLaboralApi/Models/SubRubro.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VLaboralApi.Models
+{
+    public class Subrubro
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+
+        public Rubro Rubro { get; set; }
+    }
+}
