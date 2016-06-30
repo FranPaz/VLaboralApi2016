cfd27195993756df5bb8ccd1de60fd71f77fecb0
SantiLuna <santi_luna@hotmail.com>
2016-06-30 13:04:08 -0300

    Service: Objeto para paginación

---
+++ VLaboralApi/Services/CustomPaginateResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VLaboralApi.Services
+{
+    public class CustomPaginateResult<T> where T : class
+    {
+        public int CurrentPage { get; set; }
+
+        public int PageSize { get; set; }
+
+        public int TotalRows { get; set; }
+
+        public int TotalPages { get; set; }
+
+        public IEnumerable<T> Results { get; set; }
+    }
+}
