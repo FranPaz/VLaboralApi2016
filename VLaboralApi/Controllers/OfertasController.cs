a49394161a45769328b2057cdc9c3b5ff1d99a5a
SantiLuna <santi_luna@hotmail.com>
2016-06-30 13:04:51 -0300

    Ofertas: Traer ofertas incluidas en objeto de paginacion

--- VLaboralApi/Controllers/OfertasController.cs
+++ VLaboralApi/Controllers/OfertasController.cs
@@ -9,6 +9,7 @@ using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Description;
 using VLaboralApi.Models;
+using VLaboralApi.Services;
 
 namespace VLaboralApi.Controllers
 {
@@ -20,6 +21,40 @@ namespace VLaboralApi.Controllers
         public IQueryable<Oferta> GetOfertas()
         {
             return db.Ofertas;
+        }
+
+        // GET: api/Ofertas?page=4&rows=50
+        [ResponseType(typeof(CustomPaginateResult<Oferta>))]
+        public IHttpActionResult GetOfertas(int page, int rows)
+        {
+            try
+            {
+                //SLuna: Cuento la cantidad de Ofertas vigentes que hay cargadas.
+                //TODO: Habría que agregar el estado de la oferta y algunos parametros más para asegurarse de que está activa
+                //TODO: Podríamos definir este filtro para no andar configurandolo en cada llamada. Ej: db.Ofertas.Activas() o algo de ese estilo
+                var totalRows = db.Ofertas.Count(o => DateTime.ParseExact(o.FechaInicioConvocatoria, "dd/MM/yyy", null) <= DateTime.Now && DateTime.ParseExact(o.FechaFinConvocatoria, "dd/MM/yyy", null) >= DateTime.Now);
+                var totalPages = (int)Math.Ceiling((double)totalRows / rows);
+                var results = db.Ofertas
+                    .Where(o => DateTime.ParseExact(o.FechaInicioConvocatoria, "dd/MM/yyy", null) <= DateTime.Now && DateTime.ParseExact(o.FechaFinConvocatoria, "dd/MM/yyy", null) >= DateTime.Now)
+                    .OrderBy(o => o.Id)
+                    .Skip((page - 1) * rows) //SLuna: -1 Para manejar indice(1) en pagina
+                    .Take(rows)
+                    .ToList();
+                if (!results.Any()) { return NotFound(); } //SLuna: Si no tienes elementos devuelvo 404
+
+                var result = new CustomPaginateResult<Oferta>()
+                {
+                    PageSize = rows,
+                    TotalRows = totalRows,
+                    TotalPages = totalPages,
+                    CurrentPage = page,
+                    Results = results
+                };
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            { return BadRequest(ex.Message); }
         }
 
         // GET: api/Ofertas/5
@@ -44,7 +79,7 @@ namespace VLaboralApi.Controllers
                 var prof = (from p in db.Profesionals
                             where p.Id == prmIdProfesional
                             select p)
-                            .Include(s=>s.Subrubros)
+                            .Include(s => s.Subrubros)
                             .FirstOrDefault();
 
                 //fpaz: armo un array solo con los Ids de los subrubros, sirve para el where en las ofertas
@@ -56,12 +91,12 @@ namespace VLaboralApi.Controllers
                 {
                     //fpaz: obtengo el listado de ofertas que tengan al menos un puesto con algun subrubro cargado por el empleado
                     listOfertas = (from p in db.Puestos
-                                       join o in db.Ofertas
-                                       on p.OfertaId equals o.Id
-                                       where
-                                           //DateTime.Parse(o.FechaFinConvocatoria).CompareTo(DateTime.Now) > 0  && 
-                                       p.Subrubros.Any(s => subs.Contains(s.Id)) // consulto si existe algunos de los subrubros de los puestos que este contenido dentro del array de Ids de Subrubros del Empleado
-                                       select o)
+                                   join o in db.Ofertas
+                                   on p.OfertaId equals o.Id
+                                   where
+                                   //DateTime.Parse(o.FechaFinConvocatoria).CompareTo(DateTime.Now) > 0  && 
+                                   p.Subrubros.Any(s => subs.Contains(s.Id)) // consulto si existe algunos de los subrubros de los puestos que este contenido dentro del array de Ids de Subrubros del Empleado
+                                   select o)
                              .Take(10) //fpaz: cantidad de ofertas a devolver
                              .ToList();
                 }
@@ -77,7 +112,7 @@ namespace VLaboralApi.Controllers
             catch (Exception ex)
             {
                 return BadRequest(ex.Message);
-            }            
+            }
         }
 
         // PUT: api/Ofertas/5
